6f7642ea9f7025f9472dae47113a67f65bf304bf
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2021-10-19 20:33:46 -0400

    Update packages

--- test/.mocharc.js
+++ test/.mocharc.js
@@ -1,9 +1,14 @@
 module.exports = {
   diff: true,
+  'inline-diffs': false,
   colors: true,
   extension: ['ts'],
+  parallel: true,
   require: [
-    'ts-node/register', './test/setup.js'
+    'ts-node/register',
+    './test/setup.js'
   ],
-  'watch-files': ['./test/**/*.spec.ts'],
+  slow: 75,
+  spec: 'test/**/*.spec.ts',
+  'watch-files': 'test/**/*.spec.ts'
 };
